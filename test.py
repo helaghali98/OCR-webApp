4434280bd816c798332f0d136eba51f955e88790
helaghali98 <72885523+helaghali98@users.noreply.github.com>
2021-11-14 10:51:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,11 @@ from app import app
 def test1():
     response = app.test_client().get('/')
     assert response.status_code == 200
+def test2():
+    response = app.test_client().get('/home')
+    assert response.status_code == 200
 
+def test3():
+    response = app.test_client().get('/upload')
+    assert response.status_code == 200
 
